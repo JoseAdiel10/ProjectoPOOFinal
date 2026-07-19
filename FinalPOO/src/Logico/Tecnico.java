eab7450c3c8ceb9e71cc7c05dde5ea1663766ac9
baller <baller@AD-LAPTOP>
2026-07-19 00:15:57 -0400

    Creando Clase Tecnico

---
+++ FinalPOO/src/Logico/Tecnico.java
@@ -0,0 +1,66 @@
+package Logico;
+
+/**
+ * Representa a un tecnico, heredando de la clase Persona.
+ */
+public class Tecnico extends Persona {
+    private int anoDeExperiencia;
+    private String tipoDeTecnico;
+    private boolean empleado;
+    
+    /**
+     * Constructor por defecto para inicializar un tecnico.
+     */
+    public Tecnico() {
+        super();
+        this.anoDeExperiencia = ConstantesGlobales.VALOR_NUMERICO_CERO;
+    }
+
+    /**
+     * Obtiene los anos de experiencia del tecnico.
+     * @return Entero con los anos de experiencia.
+     */
+    public int getAnoDeExperiencia() {
+        return anoDeExperiencia;
+    }
+
+    /**
+     * Establece los anos de experiencia del tecnico.
+     * @param anoDeExperiencia Entero con la cantidad de anos.
+     */
+    public void setAnoDeExperiencia(int anoDeExperiencia) {
+        this.anoDeExperiencia = anoDeExperiencia;
+    }
+
+    /**
+     * Obtiene el tipo de tecnico.
+     * @return Cadena con el tipo de especialidad.
+     */
+    public String getTipoDeTecnico() {
+        return tipoDeTecnico;
+    }
+
+    /**
+     * Establece el tipo de tecnico.
+     * @param tipoDeTecnico Cadena con el tipo de tecnico.
+     */
+    public void setTipoDeTecnico(String tipoDeTecnico) {
+        this.tipoDeTecnico = tipoDeTecnico;
+    }
+
+    /**
+     * Verifica si el tecnico esta empleado.
+     * @return Verdadero si esta empleado, falso en caso contrario.
+     */
+    public boolean isEmpleado() {
+        return empleado;
+    }
+
+    /**
+     * Establece el estado laboral del tecnico.
+     * @param empleado Valor booleano del estado.
+     */
+    public void setEmpleado(boolean empleado) {
+        this.empleado = empleado;
+    }
+}
