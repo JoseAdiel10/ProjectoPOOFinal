577acd23b3014d3da0cb47703ccdcae53eb5c19d
baller <baller@AD-LAPTOP>
2026-07-21 22:55:02 -0400

    Arreglos de errores y serializado

--- FinalPOO/src/Logico/CentroEmpleador.java
+++ FinalPOO/src/Logico/CentroEmpleador.java
@@ -1,102 +1,114 @@
 package Logico;
 
-/*Entidad que representa un centro empleador o empresa*/
-public class CentroEmpleador 
-{
-	    /*RNC que identifica al centro empleador*/
-	    private String rnc;
+import java.io.Serializable;
 
-	    /* Nombre del centro empleador*/
-	    private String nombre;
+/**
+ * Entidad que representa un centro empleador o empresa.
+ */
+public class CentroEmpleador implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    private String rnc;
+    private String nombre;
+    private float reputacion;
+    private String sector;
+    private String direccion;
+    public CentroEmpleador() {
+        this.reputacion = ConstantesGlobales.VALOR_FLOTANTE_CERO;
+    }
 
-	    /*Reputacion del centro empleador*/
-	    private float reputacion;
+    /**
+     * Constructor parametrizado para inicializar el centro empleador.
+     * @param rnc Cadena de texto con el RNC.
+     * @param nombre Cadena de texto con el nombre.
+     * @param sector Cadena de texto con el sector.
+     * @param direccion Cadena de texto con la direccion.
+     */
+    public CentroEmpleador(String rnc, String nombre, String sector, String direccion) {
+        this.rnc = rnc;
+        this.nombre = nombre;
+        this.sector = sector;
+        this.direccion = direccion;
+        this.reputacion = ConstantesGlobales.VALOR_FLOTANTE_CERO;
+    }
 
-	    /*Sector al que pertenece la empresa*/
-	    private String sector;
+    /**
+     * Obtiene el RNC del centro empleador.
+     * @return Cadena con el RNC.
+     */
+    public String getRnc() {
+        return rnc;
+    }
 
-	    /*Direccion del centro empleador*/
-	    private String direccion;
+    /**
+     * Obtiene el nombre del centro empleador.
+     * @return Cadena con el nombre.
+     */
+    public String getNombre() {
+        return nombre;
+    }
 
+    /**
+     * Obtiene la reputacion del centro empleador.
+     * @return Valor flotante con la reputacion.
+     */
+    public float getReputacion() {
+        return reputacion;
+    }
 
-	        /*Constructor por defecto para inicializar el centro empleador*/
-	         
-	        public CentroEmpleador() 
-	        {
-	            this.reputacion = ConstantesGlobales.VALOR_DECIMAL_CERO;
-	        }
+    /**
+     * Obtiene el sector del centro empleador.
+     * @return Cadena con el sector.
+     */
+    public String getSector() {
+        return sector;
+    }
 
-	    
-	    
-	    /*
-	     *Obtiene el RNC del centro empleador
-	      @return Cadena con el RNC
-	     */
-	    public String getRnc() 
-	    {
-	        return rnc;
-	    }
+    /**
+     * Obtiene la direccion del centro empleador.
+     * @return Cadena con la direccion.
+     */
+    public String getDireccion() {
+        return direccion;
+    }
 
-	    /*
-	     Obtiene el nombre del centro empleado
-	     @return Cadena con el nombre
-	    */
-	    public String getNombre() 
-	    {
-	        return nombre;
-	    }
+    /**
+     * Establece el RNC del centro empleador.
+     * @param rnc Cadena de texto con el RNC.
+     */
+    public void setRnc(String rnc) {
+        this.rnc = rnc;
+    }
 
-	    /*
-	     Obtiene la reputacion del centro empleador
-	     @return Valor flotante con la reputacion
-	    */
-	    public float getReputacion() 
-	    {
-	        return reputacion;
-	    }
-	    
-	    /*
-	     Establece el RNC del centro empleador
-	     @param rnc Cadena de texto con el RNC
-	    */
-	    public void setRnc(String rnc) 
-	    {
-	        this.rnc = rnc;
-	    }
+    /**
+     * Establece el nombre del centro empleador.
+     * @param nombre Cadena de texto con el nombre.
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
-	    /*
-	     Establece el nombre del centro empleador
-	     @param nombre Cadena de texto con el nombre
-	    */
-	    public void setNombre(String nombre) 
-	    {
-	        this.nombre = nombre;
-	    }
-	    
-	    /*
-	     Establece la reputacion del centro empleador.
-	     @param reputacion Valor flotante a asignar.
-	    */
-	    public void setReputacion(float reputacion) 
-	    {
-	        this.reputacion = reputacion;
-	    }
+    /**
+     * Establece la reputacion del centro empleador.
+     * @param reputacion Valor flotante a asignar.
+     */
+    public void setReputacion(float reputacion) {
+        this.reputacion = reputacion;
+    }
 
-	    /*
-	     Establece el sector del centro empleador.
-	     @param sector Cadena de texto con el sector.
-	    */
-	    public void setSector(String sector) 
-	    {
-	        this.sector = sector;
-	    }
+    /**
+     * Establece el sector del centro empleador.
+     * @param sector Cadena de texto con el sector.
+     */
+    public void setSector(String sector) {
+        this.sector = sector;
+    }
 
-	    /*
-	      Establece la direccion del centro empleador.
-	      @param direccion Cadena de texto con la direccion.
-	    */
-	    public void setDireccion(String direccion) 
-	    {
-	        this.direccion = direccion;
-	    }
+    /**
+     * Establece la direccion del centro empleador.
+     * @param direccion Cadena de texto con la direccion.
+     */
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
 }
