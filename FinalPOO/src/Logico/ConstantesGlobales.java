10032bb591ee448e35973bf7406ebe79f312b180
baller <baller@AD-LAPTOP>
2026-07-21 22:53:19 -0400

    Implemetacion de mas constantes para mejorar el funcionamiento

--- FinalPOO/src/Logico/ConstantesGlobales.java
+++ FinalPOO/src/Logico/ConstantesGlobales.java
@@ -5,6 +5,9 @@ package Logico;
  */
 public class ConstantesGlobales {
     public static final int VALOR_NUMERICO_CERO = 0;
-    public static final double VALOR_doubleDECIMAL_CERO = 0.0;
-    public static final float VALOR_DECIMAL_CERO = 0;
+    public static final double VALOR_DECIMAL_CERO = 0.0;
+    public static final float VALOR_FLOTANTE_CERO = 0.0f;
+    public static final double PUNTAJE_CERO = 0.0;
+    public static final double PUNTAJE_PARCIAL = 50.0;
+    public static final double PUNTAJE_MAXIMO = 100.0;
 }
