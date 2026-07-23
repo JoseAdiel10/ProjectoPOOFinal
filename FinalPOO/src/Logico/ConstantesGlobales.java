2dd18f7193484d78a07db194f1305694ce0e6517
baller <baller@AD-LAPTOP>
2026-07-22 23:43:29 -0400

    Constantes nuevas para sistema de puntaje

--- FinalPOO/src/Logico/ConstantesGlobales.java
+++ FinalPOO/src/Logico/ConstantesGlobales.java
@@ -8,6 +8,11 @@ public class ConstantesGlobales {
     public static final double VALOR_DECIMAL_CERO = 0.0;
     public static final float VALOR_FLOTANTE_CERO = 0.0f;
     public static final double PUNTAJE_CERO = 0.0;
-    public static final double PUNTAJE_PARCIAL = 50.0;
-    public static final double PUNTAJE_MAXIMO = 100.0;
+    
+    public static final double PUNTAJE_COINCIDENCIA_MAXIMA = 30.0;
+    public static final double PUNTAJE_COINCIDENCIA_MUY_ALTA = 25.0;
+    public static final double PUNTAJE_COINCIDENCIA_ALTA = 20.0;
+    public static final double PUNTAJE_COINCIDENCIA_MEDIA = 10.0;
+    public static final double PUNTAJE_COINCIDENCIA_BAJA = 5.0;
+    public static final double PUNTAJE_POR_ANO_EXPERIENCIA = 1.0;
 }
