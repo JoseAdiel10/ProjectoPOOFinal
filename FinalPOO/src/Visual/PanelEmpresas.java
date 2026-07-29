d78329b0d8d8980d32c83280c377136099a50867
cruzm <cruzm@LAPTOP-CG4KLFMH>
2026-07-29 07:51:07 -0400

    Clase visual de empresas creada

---
+++ FinalPOO/src/Visual/PanelEmpresas.java
@@ -0,0 +1,36 @@
+package Visual;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
+
+import Logico.CentroEmpleador;
+import excepciones.ExcepcionNoEliminable;
+
+/**
+ * Panel para registrar, consultar, modificar y eliminar centros empleadores.
+ */
+public class PanelEmpresas extends JPanel
+
+{
+	
+	
+	
+	
+}
