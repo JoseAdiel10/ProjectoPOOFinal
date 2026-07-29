00b31a14224f28fecd30d50c6666442f09665072
kiara17mari-byte <kiara17mari@gmail.com>
2026-07-29 00:45:36 -0400

    agregue atributos para la navegación de paneles

--- FinalPOO/src/Visual/Principal.java
+++ FinalPOO/src/Visual/Principal.java
@@ -8,10 +8,22 @@ public class Principal extends JFrame {
     public static final Color COLOR_PRIMARIO = new Color(46, 92, 138);
     public static final Color COLOR_ACENTO = new Color(255, 160, 60);
     public static final Color COLOR_TEXTO = new Color(35, 40, 48);
+    
 
     private Bolsa bolsa;
     private Usuario usuarioActual;
 
+    private JPanel panelContenedor;
+    private CardLayout cardLayout;
+
+    private PanelLogin panelLogin;
+    private PanelMenu panelMenu;
+    private PanelPersonas panelPersonas;
+    private PanelEmpresas panelEmpresas;
+    private PanelVacantes panelVacantes;
+    private PanelPostulaciones panelPostulaciones;
+    private PanelMatching panelMatching;
+    
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
             public void run() {
