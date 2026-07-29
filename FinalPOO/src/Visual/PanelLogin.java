eb5caf46244f216f7b5d22335c87578ed8cd7c8f
kiara17mari-byte <kiara17mari@gmail.com>
2026-07-29 00:59:49 -0400

    implemente la estructura inicial del panel de inicio de sesión y configurar su diseño base

--- FinalPOO/src/Visual/PanelLogin.java
+++ FinalPOO/src/Visual/PanelLogin.java
@@ -1,5 +1,16 @@
 package FinalPOO.src.Visual;
 
-public class PanelLogin {
+public class PanelLogin extends JPanel {
 
+    private static final long serialVersionUID = 1L;
+
+    private Principal ventana;
+    private JTextField txtUsuario;
+    private JPasswordField txtClave;
+
+    public PanelLogin(Principal ventana) {
+        this.ventana = ventana;
+        setLayout(new BorderLayout());
+        setBackground(Principal.COLOR_FONDO);
+        
 }
