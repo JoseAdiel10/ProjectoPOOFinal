3f0022d840b4993b62da0f3f8d07b652b6a1e342
kiara17mari-byte <kiara17mari@gmail.com>
2026-07-29 22:25:25 -0400

    modifique los colores existentes y agregaron las constantes

--- FinalPOO/src/Visual/Principal.java
+++ FinalPOO/src/Visual/Principal.java
@@ -19,11 +19,13 @@ public class Principal extends JFrame {
 
     private static final long serialVersionUID = 1L;
 
-    public static final Color COLOR_FONDO = new Color(245, 247, 250);
-    public static final Color COLOR_PRIMARIO = new Color(46, 92, 138);
-    public static final Color COLOR_ACENTO = new Color(255, 160, 60);
-    public static final Color COLOR_TEXTO = new Color(35, 40, 48);
-
+    public static final Color COLOR_FONDO = new Color(244, 246, 249);
+    public static final Color COLOR_PRIMARIO = new Color(23, 78, 115);
+    public static final Color COLOR_PRIMARIO_OSCURO = new Color(15, 55, 82);
+    public static final Color COLOR_ACENTO = new Color(255, 111, 97);
+    public static final Color COLOR_EXITO = new Color(46, 160, 110);
+    public static final Color COLOR_TEXTO = new Color(31, 41, 51);
+    
     private Bolsa bolsa;
     private Usuario usuarioActual;
 
