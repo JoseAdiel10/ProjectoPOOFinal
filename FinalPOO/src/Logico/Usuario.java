b17f2f8ab29a17e949def2e49d456525dc90a0df
kiara17mari-byte <kiara17mari@gmail.com>
2026-07-29 22:41:37 -0400

    modifiqu el metodo match(),que si el usuario no tenía credenciales, las creaba automáticamente

--- FinalPOO/src/Logico/Usuario.java
+++ FinalPOO/src/Logico/Usuario.java
@@ -140,10 +140,10 @@ public class Usuario implements Serializable {
      * @return Verdadero si el usuario y la contrasena coinciden.
      */
     public boolean match(String usuario, String clave) {
-    	if (this.usernameEmpresa == null) {
-            this.usernameEmpresa = usuario;
-            this.passwd = clave;
-            return true;
+    	return this.usernameEmpresa != null
+    		    && this.usernameEmpresa.equals(usuario)
+    		    && this.passwd != null
+    		    && this.passwd.equals(clave);
         }
     	
         return this.usernameEmpresa != null && this.usernameEmpresa.equals(usuario)
